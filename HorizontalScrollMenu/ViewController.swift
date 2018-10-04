5fe6819b5c43228b592c3b17dc5707e279436972
Arun Saxena <admin@admins-macbook-pro.local>
2018-10-04 13:04:45 +0530

    Initial Commit

---
+++ HorizontalScrollMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HorizontalScrollMenu
+//
+//  Created by Admin on 04/10/18.
+//  Copyright © 2018 Equitronix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
